e211148a09b16a6b37a93dea85bc821c0eed6262
tsuny221 <kingoftsuny@gmail.com>
2020-08-26 13:07:53 +0900

    [Finish] project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+    // void Update()
+    // {
+    //     //左矢印が押された時
+    //     if(Input.GetKeyDown(KeyCode.LeftArrow))
+    //     {
+    //         transform.Translate(-3, 0, 0); //左「３」動かす
+    //     }
+    //     //右矢印が押された時
+    //     if(Input.GetKeyDown(KeyCode.RightArrow))
+    //     {
+    //         transform.Translate(3, 0, 0); //右「３」動かす
+    //     }
+    // }
+}
